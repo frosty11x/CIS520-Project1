085e80467972fe0a2c1bee54333840ca483ebb68
Noah Livingston <noahl@fusion.cis.ksu.edu>
2014-10-03 12:23:38 -0500

    Noah

---
+++ bochs-2.4.5/bxversion.h
@@ -0,0 +1,7 @@
+/////////////////////////////////////////////////////////////////////////
+// This file is checked in as bxversion.h.in.  The configure script
+// substitutes variables and creates bxversion.h.
+/////////////////////////////////////////////////////////////////////////
+
+#define VER_STRING "2.4.5"
+#define REL_STRING "Build from CVS snapshot, on April 25, 2010"
---
+++ pintos/src/threads/README.txt
@@ -0,0 +1,11 @@
+To incorporate alarm_mega, the following changes were required:
+
+CREATE:
+alarm-mega.ck (Test call)
+
+UPDATE:
+tests.c (Add test function to list of tests)
+tests.h (Add test header)
+Rubric.alarm (Add test to rubric)
+Make.tests (Add test to build file)
+alarm-wait.c (Add actual test function)
